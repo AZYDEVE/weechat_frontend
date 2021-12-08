84640bc4606676cb64a0400af8431aeb9b4373f0
Alex Yang <71572454+AZYDEVE@users.noreply.github.com>
2021-12-09 04:56:46 +0800

    changes

--- weechat_frontend/src/chatPage/ChatPage.js
+++ weechat_frontend/src/chatPage/ChatPage.js
@@ -3,49 +3,65 @@ import SideBar from "../components/sideBar/ChatRoomSideBar";
 import ChatRoom from "../components/chatRoom/ChatRoom";
 import useAuthInfo from "../utils/userUtil";
 import OpenSocket from "../utils/webSocket";
-import { listGroups } from "../services/relationship_api";
+import {
+  listGroups,
+  getChatRoomHistory,
+  addGroup,
+} from "../services/relationship_api";
 import "./chatPage.css";
 
 const ChatPage = () => {
   const ws = OpenSocket();
+  const userInfo = useAuthInfo();
   const [listOfChatRoom, setListOfChatRoom] = useState([]);
   const [chatRoomID, setChatRoomID] = useState("");
   const [listOfMessages, setListOfMessages] = useState([]);
 
   ws.onmessage = function (event) {
     const message = JSON.parse(event.data);
-    // console.log(message);
+    console.log(message);
     setListOfMessages((current) => [...current, message]);
   };
 
+  const getListOfChatRooms = async () => {
+    //this function is to fetch from server a list of chatRoom associated with the userID
+    console.log(userInfo.email);
+    try {
+      const listOfChatRoom = await listGroups(userInfo.email);
+      setListOfChatRoom(listOfChatRoom);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //example if chatroom json
   const listOfChatRoomObj = [
     { name: "chatroom A", Id: "A1" },
     { name: "chatroom B", Id: "B1" },
     { name: "chatroom C", Id: "C1" },
   ]; // delete: temp for testing
 
-  const userInfo = useAuthInfo();
-
   useEffect(() => {
-    if (userInfo.user_id !== "") {
-      setListOfChatRoom(listOfChatRoomObj);
+    console.log(userInfo);
+    if (userInfo.email !== "") {
+      // setListOfChatRoom(listOfChatRoomObj);
+      try {
+        getListOfChatRooms();
+      } catch (error) {
+        console.log(error);
+      }
     }
-  }, []);
+  }, [userInfo]);
 
-  const getListOfChatRooms = async () => {
-    //this function is to fetch from server a list of chatRoom associated with the userID
-    const listOfChatRoom = await listGroups({ userId: userInfo.email });
-    setListOfChatRoom(listOfChatRoom);
-  };
-
-  const selectChatRoom = (chatRoomID) => {
+  const selectChatRoom = async (chatRoomID) => {
     console.log(chatRoomID);
     setChatRoomID(chatRoomID);
     ws.send(JSON.stringify({ type: "newtopic", body: chatRoomID }));
     setListOfMessages([]);
+    //get chat history
+    const chatHistory = await getChatRoomHistory(chatRoomID);
+    setListOfMessages(chatHistory);
   };
-
-  const getChatHistory = async () => {};
 
   const appenNewChatRoom = (newChatRoom) => {
     setListOfChatRoom([...listOfChatRoom, newChatRoom]);
--- weechat_frontend/src/services/relationship_api.js
+++ weechat_frontend/src/services/relationship_api.js
@@ -4,7 +4,7 @@ import { url } from "../Constant/url.js";
 // https://auth0.com/docs/users/user-search/retrieve-users-with-get-users-endpoint
 // this hardcoded api_token will be expired on Dec 17 2021 at noon
 
-export async function addGroup({ groupName, userList }) {
+export async function addGroup(groupName, userList) {
   return axios
     .post(`${url}/dbms/relationship/add`, {
       groupName: groupName,
@@ -18,15 +18,8 @@ export async function addGroup({ groupName, userList }) {
     });
 }
 
-export async function listGroups({ userId }) {
-  return axios
-    .post(`${url}/dbms/relationship/list`, { userId: userId })
-    .then((res) => {
-      return res;
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+export async function listGroups(userId) {
+  return await axios.post(`${url}/dbms/relationship/list`, { userId: userId });
 }
 
 // need to change the url
